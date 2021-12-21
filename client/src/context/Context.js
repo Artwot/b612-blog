31a9f95b015a53d0c49c7b4952876090b3d795e8
Jorge Arturo Sarabia <jsarabiar98@gmail.com>
2021-12-21 14:30:13 -0600

    Pages are working. Some details & problems are pending to fix them

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -2,7 +2,7 @@ import { createContext, useReducer, useEffect } from "react";
 import Reducer from "./Reducer";
 
 const INITIAL_STATE = {
-  username:  null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
   isFetching: false,
   error: false,
 };
@@ -13,7 +13,6 @@ export const ContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(state.user));
-    console.log(state.user)
   }, [state.user]);
 
   return (
